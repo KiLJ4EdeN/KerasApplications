ff98b687ba13899045a2c09e0f07c6c750473a6a
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-12-05 10:17:42 +0330

    Create resnet50v2.py

---
+++ models/resnet50v2.py
@@ -0,0 +1,105 @@
+from tensorflow.keras import models, layers
+
+
+def stack2(x, filters, blocks, stride1=2, name=None):
+    x = block2(x, filters, conv_shortcut=True, name=name + '_block1')
+    for i in range(2, blocks):
+        x = block2(x, filters, name=name + '_block' + str(i))
+    x = block2(x, filters, stride=stride1, name=name + '_block' + str(blocks))
+    return x
+
+
+def block2(x, filters, kernel_size=3, stride=1,
+           conv_shortcut=False, name=None):
+    bn_axis = 3
+
+    preact = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                       name=name + '_preact_bn')(x)
+    preact = layers.Activation('relu', name=name + '_preact_relu')(preact)
+
+    if conv_shortcut is True:
+        shortcut = layers.Conv2D(4 * filters, 1, strides=stride,
+                                 name=name + '_0_conv')(preact)
+    else:
+        shortcut = layers.MaxPooling2D(1, strides=stride)(x) if stride > 1 else x
+
+    x = layers.Conv2D(filters, 1, strides=1, use_bias=False,
+                      name=name + '_1_conv')(preact)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                  name=name + '_1_bn')(x)
+    x = layers.Activation('relu', name=name + '_1_relu')(x)
+
+    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name=name + '_2_pad')(x)
+    x = layers.Conv2D(filters, kernel_size, strides=stride,
+                      use_bias=False, name=name + '_2_conv')(x)
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                  name=name + '_2_bn')(x)
+    x = layers.Activation('relu', name=name + '_2_relu')(x)
+
+    x = layers.Conv2D(4 * filters, 1, name=name + '_3_conv')(x)
+    x = layers.Add(name=name + '_out')([shortcut, x])
+    return x
+
+
+def ResNet(stack_fn,
+           preact,
+           use_bias,
+           model_name='resnet',
+           include_top=True,
+           input_shape=None,
+           pooling=None,
+           classes=1000):
+
+    img_input = layers.Input(shape=input_shape)
+    bn_axis = 3
+    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)), name='conv1_pad')(img_input)
+    x = layers.Conv2D(64, 7, strides=2, use_bias=use_bias, name='conv1_conv')(x)
+
+    if preact is False:
+        x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                      name='conv1_bn')(x)
+        x = layers.Activation('relu', name='conv1_relu')(x)
+
+    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name='pool1_pad')(x)
+    x = layers.MaxPooling2D(3, strides=2, name='pool1_pool')(x)
+
+    x = stack_fn(x)
+
+    if preact is True:
+        x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                      name='post_bn')(x)
+        x = layers.Activation('relu', name='post_relu')(x)
+
+    if include_top:
+        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        x = layers.Dense(classes, activation='softmax', name='probs')(x)
+    else:
+        if pooling == 'avg':
+            x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        elif pooling == 'max':
+            x = layers.GlobalMaxPooling2D(name='max_pool')(x)
+
+    # Create model.
+    model = models.Model(img_input, x, name=model_name)
+    return model
+
+
+def ResNet50V2(include_top=True,
+               input_shape=(224, 224, 3),
+               pooling=None,
+               classes=1000):
+    def stack_fn(x):
+        x = stack2(x, 64, 3, name='conv2')
+        x = stack2(x, 128, 4, name='conv3')
+        x = stack2(x, 256, 6, name='conv4')
+        x = stack2(x, 512, 3, stride1=1, name='conv5')
+        return x
+    return ResNet(stack_fn, True, True, 'resnet50v2',
+                  include_top, input_shape,
+                  pooling, classes,)
+
+
+if __name__ == '__main__':
+    mod = ResNet50V2()
+    print(mod.summary())
+
